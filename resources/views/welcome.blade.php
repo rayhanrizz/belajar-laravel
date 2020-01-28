bc7407aedfbaf213e2444a34ea4a6112ce2d84f9
rayhanrizz <rayhanriz@gmail.com>
2020-01-28 11:12:06 +0700

    project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Air Jordan Shop</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+          <center><img src="img/lo.png" width="200"></center>
+  <nav class="navbar navbar-default ">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1 ">
+      <ul class="nav navbar-nav ">
+        <li class=""><a href="#">Home<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Galeri</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">T-Shirt</a></li>
+        <li><a href="#">Hoodie</a></li>
+        <li><a href="#">Shoes</a></li>
+      </ul>
+        <form class="navbar-form navbar-right">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+    <div class="row top">
+      <div class="col-md-6">
+          <p><center><img src="img/l.png" width="400"></center></p>
+    </div >
+    <div class="col-md-6">
+      <h1>Welcome to Jordan Shop</h1>
+      <p>
+      Air Jordan telah menjadi bagian penting dalam sejarah basket, bahkan sejarah sneaker sejak pertama kali diperkenalkan pada tahun 1985. Banyak cerita dibalik sneaker ini, mulai dilarang dipakai untuk bertanding di NBA, hingga orang-orang percaya bisa terbang dengan memakai sneaker ini (Air Jordan III). Selain bersejarah, Air Jordan juga meninggalkan kenangan bagi pemiliknya.
+
+      Air Jordan adalah merek sneaker basket dan perlengkapan olahraga yang diproduksi Nike. Merek ini khusus diciptakan untuk pemain basket paling fenomenal di dunia, Michael Jordan. Sneaker Air Jordan I yang asli, diproduksi untuk Michael Jordan pada tahun 1984. Tapi baru diluncurkan ke pasaran tahun 1985. Sneaker-sneaker Air Jordan dirancang oleh Peter Moore, Tinker Hatfield dan Bruce Kilgore. Percaya atau tidak, hingga saat ini Nike masih memproduksi semua varian Air Jordan
+
+      Sejak diperkenalkan ke pasar sneaker olahraga, Air Jordan berevolusi dari sneaker basket asli ke berbagai model dan kegunaan berbeda. Ini ditunjukkan dengan varian I hingga XXXI. Air Jordan juga sudah menunjuk 21 pemain NBA aktif sebagai duta, termasuk Jimmy Butler, Chris Paul, Blake Griffin, Kawhi Leonard, Russell Westbrook dan Carmelo Anthony. Jordan juga memproduksi beberapa sneaker untuk cabang olahraga lain seperti NFL, MLB hingga NASCAR.
+
+      </p>
+      <div class="btn-group" role="group" aria-label="...">
+      <button type="button" class="btn btn-default"> Shop Now </button>
+    </div>
+    </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <p>   </p>
+  </div>
+   <div class="col-md-2">
+      <div class="">
+        <div class="">
+          <h2><span class="" aria-hidden="true"></span></h2>
+          
+        </div>
+      </div>
+  </div>
+   <div class="col-md-2">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/jo.png" width="150">
+          <p>Air Jordan Shoes</p>
+          <button type="button" class="btn btn-default " style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+   <div class="col-md-2">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/h.jpg" width="92">
+          <p>Air Jordan Hoodie</p>
+          <button type="button" class="btn btn-default " style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+   <div class="col-md-2">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/t.png" width="102">
+          <p>Air Jordan T-shirt</p>
+          <button type="button" class="btn btn-default" style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+  <div class="col-md-2">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/p.jpg" width="95">
+          <p>Air Jordan Hat</p>
+          <button type="button" class="btn btn-default" style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+  <div class="col-md-2">
+      <div class="t">
+        <div class="">
+          <h2><span class=" aria-hidden="true"></span></h2>
+        </div>
+      </div>
+  </div>
+  </div>
+  <div class="row top">
+          <div class="col-md-12">
+          <h3><center>Featured Product</center></h3>
+  </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+      <div class="">
+        <div class=" ">
+          <h2><span class="" aria-hidden="true"></span></h2>
+            <p> </p>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-2">
+      <div class="">
+        <div class="">
+        </div>
+      </div>
+  </div>
+   <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/j1.jpg" width="150">
+          <p>Air Jordan 1</p>
+          <button type="button" class="btn btn-default " style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+   <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/2.jpg" width="150">
+          <p>Air Jordan 3 Black Cement</p>
+          <button type="button" class="btn btn-default " style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+   <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/3.png" width="165">
+          <p>Air Jordan 11 Space Jam</p>
+          <button type="button" class="btn btn-default" style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+  <div class="col-md-2">
+      <div class="t">
+        <div class="">
+          <h2><span class=" aria-hidden="true"></span></h2>
+        </div>
+      </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="">
+        <div class=" ">
+          <h2><span class="" aria-hidden="true"></span></h2>
+            <p> </p>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-2">
+      <div class="">
+        <div class="">
+        </div>
+      </div>
+  </div>
+   <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/hj.jpg" width="150">
+          <p>Air Jordan Hoodie Logo</p>
+          <button type="button" class="btn btn-default " style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+   <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/hs.jpg" width="171">
+          <p>Air Jordan X Supreme Hoodie</p>
+          <button type="button" class="btn btn-default " style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+   <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/jp.jpg" width="120">
+          <p>Air Jordan X Paris Hoodie</p>
+          <button type="button" class="btn btn-default" style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+  <div class="col-md-2">
+      <div class="t">
+        <div class="">
+          <h2><span class=" aria-hidden="true"></span></h2>
+        </div>
+      </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="">
+        <div class=" ">
+          <h2><span class="" aria-hidden="true"></span></h2>
+            <p> </p>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-2">
+      <div class="">
+        <div class="">
+        </div>
+      </div>
+  </div>
+   <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/sb.jpg" width="150">
+          <p>Air Jordan SnapBack</p>
+          <button type="button" class="btn btn-default " style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+   <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/base.jpg" width="153">
+          <p>Air Jordan Baseball Cap</p>
+          <button type="button" class="btn btn-default " style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+   <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-body text-center">
+          <img src="img/sup.jpg" width="155">
+          <p>Air Jordan X Supreme SnapBack</p>
+          <button type="button" class="btn btn-default" style="background-color: aqua;"> Shop </button>
+        </div>
+      </div>
+  </div>
+  <div class="col-md-2">
+      <div class="t">
+        <div class="">
+          <h2><span class=" aria-hidden="true"></span></h2>
+        </div>
+      </div>
+  </div>
+</div>
+<div class="row top">
+          <div class="col-md-12">
+          <table align="center">
+            <tr>
+              <td style="padding-left: 100px;"><h4>Featured Sale</h4></td>
+              <td style="padding-left: 100px;"><h4>For Men</h4></td>
+              <td style="padding-left: 100px;"><h4>For Women</h4></td>
+            </tr>
+            <tr>
+              <td style="padding-left: 100px;">Shoes</td>
+              <td style="padding-left: 100px;">Shoes</td>
+              <td style="padding-left: 100px;">Tshirt</td>
+            </tr>
+            <tr>
+              <td style="padding-left: 100px;">Tshirt</td>
+              <td style="padding-left: 100px;">Tshirt</td>
+              <td style="padding-left: 100px;">Hoodie</td>
+            </tr>
+            <tr>
+              <td style="padding-left: 100px;">Hat</td>
+              <td style="padding-left: 100px;">Hat</td>
+              <td style="padding-left: 100px;">Crewneck</td>
+            </tr>
+            <tr>
+              <td style="padding-left: 100px;">Hoodie</td>
+              <td style="padding-left: 100px;">Hoodie</td>
+            </tr>
+            <tr>
+              <td style="padding-left: 100px;">Crewneck</td>
+              <td style="padding-left: 100px;">Crewneck</td>
+            </tr>
+            <tr>
+              <td style="padding-left: 100px;">Zip Hoodie</td>
+              <td style="padding-left: 100px;">Zip Hoodie</td>
+            </tr>
+            <tr>
+              <td style="padding-left: 100px;">Pants</td>
+              <td style="padding-left: 100px;">Pants</td>
+            </tr>
+          </table>
+  </div>
+  </div>
+<div class="container">
+  <div class="row title text-center">
+    <h4></h4>
+  </div>
+</div>
+<div class="container footercolor">
+  <div class="row title text-center">
+    <p><center>Rayhanxrizz ~ copyright &#169; 2018 All rights reserved</center></p>
+  </div>
+</div>
+  </body>
+</html>
